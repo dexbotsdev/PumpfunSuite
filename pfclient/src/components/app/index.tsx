b0e4a95e757f4bdeb8a9f54d8b7c83afcce24632
DexbotsDev <dexbotsdev@gmail.com>
2024-09-23 09:02:42 +0530

    update

--- pfclient/src/components/app/index.tsx
+++ pfclient/src/components/app/index.tsx
@@ -9,8 +9,8 @@ export default function App() {
       <KindeProvider
             clientId="3199bc25f44e4db78ac6fb4cc352c61f"
             domain="https://radialdapps.kinde.com"
-            redirectUri="http://13.70.206.19:5050/app"
-            logoutUri="http://13.70.206.19:5050/"
+            redirectUri="https://13.70.206.19:5050/app"
+            logoutUri="https://13.70.206.19:5050/"
         >
             <Router />
         </KindeProvider> 
